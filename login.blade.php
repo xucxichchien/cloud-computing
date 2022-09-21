247d5c590ff613300b19bb43f4dc3f3dd3fa6b76
xucxichchien <111264991+xucxichchien@users.noreply.github.com>
2022-09-21 11:32:12 +0700

    Add files via upload

---
+++ login.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Custom Authentication=</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="containner">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4" style="margin-top:20px;">
+                <h4>Login Here</h4>
+                <hr>
+                <form action="{{route('login-user')}}" method="post" >
+                    @if(Session::has('success'))
+                    <div class="alert alert-success">{{Session::get('success')}}</div>
+                    @endif
+                    @if(Session::has('fail'))
+                    <div class="alert alert-danger">{{Session::get('fail')}}</div>
+                    @endif
+                     @csrf
+                    <div class="form-group">
+                        <label for="emailS">Email</label>
+                        <input type="text" class="form-control" placeholder="=Enter Email" name="email" value="">
+                        <span class="tex-danger">@error('email'){{$message}}@enderror</span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="text" class="form-control" placeholder="=Enter Password" name="password" value="">
+                        <span cla ss="tex-danger">@error('password'){{$message}}@enderror</span>
+                    </div>
+                    <div class="form-group">
+                         <button class="btn btn-block btn-primary" type="submit">Login</button>
+                    </div>
+                    <br>
+                    <a href="registration">new User !! Register here</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</html>
